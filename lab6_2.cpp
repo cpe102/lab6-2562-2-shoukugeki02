2c3c0f6d05b67ef762b11db1fd1075e503fc04d9
shoukugeki02 <59277996+shoukugeki02@users.noreply.github.com>
2020-01-17 17:15:07 +0700

    Update lab6_2.cpp

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,18 +1,31 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+int ID;
+string talk;
+string name;
+string mymovie;
+string freetime;
+cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+cin >> name;
+cout << "Fahsai: Wow!!! " << name << " that is a really cool name.\n";
+cout << "Fahsai: I think you are Engineering student. What is your student ID?\n";
+cout << name <<": ";
+cin >> ID;
+int Y = ID/10000000-12;
+cout << "Fahsai: I think you may be GEAR." << Y  <<  " I have a free movie tickets for you.\n";
+cout << "Fahsai: Let's go to cinema together!!!\n";
+cout << "Fahsai: What movie do you want to watch?\n";
+cout << name << ": ";
+cin >> mymovie;
+cout << "Fahsai: So....which day are you free to go with me?\n";
+cout << name;
+cin >> freetime;
+cout << "Fahsai:" << freetime << "that is OK!!! I'm looking forward to" << mymovie << "with you.\n";
+cout  << name << ": ";
+cin >> talk;
+cout << " seeyou " << freetime << " Bye Bye \(^ ^)/ ";
+return 0;
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker that is a really cool name.
-Fahsai: I think you are Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47.  I have a free movie tickets for you. 
-Fahsai: Let's go to cinema together!!! 
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watch Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/ 
-*/
+}
